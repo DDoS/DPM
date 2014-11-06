105e2dec7dfbe13c797b125c68bfd75ea177f791
Aleksi Sapon <QcTechs@gmail.com>
2014-11-06 13:26:23 -0500

    Implement Display

--- src/Display.java
+++ src/Display.java
@@ -1,24 +1,39 @@
+import lejos.nxt.*;
+
 /**
  * A singleton class to control the Lego NXT controller brick's single LCD display. On each line the display will print one key/value mapping. Ordering can only be guaranteed when using {@link
- * #reserve(String...)} to reserve multiple lines at once. In such a case the array of mappings are guaranteed to occupy successive lines.
+ * #reserve(String...)} to reserve multiple lines at once. In such a case the array of mappings are guaranteed to occupy successive lines. Updates are drawn immediately.
  * <p/>
  * THREAD SAFE
  */
-public class Display {
-    /**
-     * Reserves successive lines for the desired array of keys.
-     *
-     * @param keys The keys to reserve
-     */
-    public static void reserve(String... keys) {
+public final class Display {
+    private static final int LINE_COUNT = LCD.SCREEN_HEIGHT / LCD.FONT_HEIGHT;
+    private static final int COLUMN_COUNT = LCD.SCREEN_WIDTH / LCD.FONT_WIDTH;
+    private static final Entry[] map = new Entry[LINE_COUNT];
+    private static int currentIndex = 0;
 
+    // Prevent instances of the singleton
+    private Display() {
     }
 
     /**
-     * Clears the display mappings. Use with caution as this will affect the way other threads will have their data displayed (potentially messing up the ordering).
+     * Reserves successive lines for the desired array of keys. Lines are allocated to keys following the array ordering, so if fewer lines than keys are available the first keys are given priority.
+     *
+     * @param keys The keys to reserve
+     * @return How many keys where successfuly reserved
      */
-    public static void clear() {
-
+    public static int reserve(String... keys) {
+        synchronized (map) {
+            if (currentIndex + keys.length > LINE_COUNT) {
+                return 0;
+            }
+            int reserved = 0;
+            for (; currentIndex < LINE_COUNT; currentIndex++, reserved++) {
+                map[currentIndex] = new Entry(keys[reserved]);
+                draw(currentIndex);
+            }
+            return reserved;
+        }
     }
 
     /**
@@ -28,17 +43,92 @@ public class Display {
      *
      * @param key The key for the mapping
      * @param val The value for the mapping
+     * @return Whether or not the update succeeded
      */
     public static boolean update(String key, String val) {
-        return false;
+        synchronized (map) {
+            for (int i = 0; i < currentIndex; i++) {
+                final Entry entry = map[i];
+                if (entry.hasKey(key)) {
+                    entry.value = val;
+                    draw(i);
+                    return true;
+                }
+            }
+            if (currentIndex < LINE_COUNT) {
+                map[currentIndex] = new Entry(key, val);
+                draw(currentIndex);
+                currentIndex++;
+                return true;
+            }
+            return false;
+        }
     }
 
     /**
-     * Removes a mapping from the display. The line is freed.
+     * Removes a mapping from the display. The line is freed. All lines bellow are shifted up.
      *
      * @param key The key of the mapping to remove
      */
     public static void remove(String key) {
+        synchronized (map) {
+            boolean deleted = false;
+            for (int i = 0; i < currentIndex; i++) {
+                if (!deleted) {
+                    final Entry entry = map[i];
+                    if (entry.hasKey(key)) {
+                        map[i] = null;
+                        deleted = true;
+                    }
+                }
+                if (deleted) {
+                    map[i] = i < currentIndex - 1 ? map[i + 1] : null;
+                    draw(i);
+                }
+            }
+            if (deleted) {
+                currentIndex--;
+            }
+        }
+    }
 
+    /**
+     * Clears the display mappings. Use with caution as this will affect the way other threads will have their data displayed (potentially messing up the ordering).
+     */
+    public static void clear() {
+        synchronized (map) {
+            for (int i = 0; i < currentIndex; i++) {
+                map[i] = null;
+                draw(i);
+            }
+            currentIndex = 0;
+        }
+    }
+
+    private static void draw(int line) {
+        LCD.clear(line);
+        final Entry entry = map[line];
+        if (entry == null) {
+            return;
+        }
+        LCD.drawString(entry.key + ": " + (entry.value != null ? entry.value : ""), 0, line);
+    }
+
+    private static class Entry {
+        private final String key;
+        private String value;
+
+        private Entry(String key) {
+            this(key, null);
+        }
+
+        private Entry(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        private boolean hasKey(String key) {
+            return this.key.equals(key);
+        }
     }
 }
