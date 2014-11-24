298364136a6f15b3467a9510d677ff6bbb358117
Aleksi Sapon <QcTechs@gmail.com>
2014-11-24 16:18:56 -0500

    Lejo's Random is broken, implement our own simple one
    It has repeating patterns
    Only returns bits, so you can only get ints in ranges of powers of two

---
+++ src/Random.java
@@ -0,0 +1,16 @@
+public class Random {
+    private static final int BITS = 48;
+    private static final long MUL = 0x5DEECE66DL;
+    private static final long ADD = 0xBL;
+    private static final long MASK = (1L << BITS) - 1;
+    private long seed;
+
+    public Random() {
+        this.seed = (System.currentTimeMillis() ^ MUL) & MASK;
+    }
+
+    public int nextBits(int bits) {
+        seed = (seed * MUL + ADD) & MASK;
+        return (int) (seed >>> BITS - bits);
+    }
+}
