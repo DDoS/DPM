7851d9473caaa1ba147b18521e3fb492c05338b2
Aleksi Sapon <QcTechs@gmail.com>
2014-11-27 13:34:17 -0500

    Add conversion from tile coords to odo coords in Tile

--- src/Tile.java
+++ src/Tile.java
@@ -2,8 +2,17 @@ public final class Tile {
     // Spacing of the tiles in centimeters
     public static final float ONE = 30.48f;
     public static final float HALF = ONE / 2;
+    public static final float QUARTER = ONE / 4;
 
     // Prevent instances of the singleton
     private Tile() {
+    }
+
+    public static float toOdo(int tile) {
+        return tile * ONE + HALF;
+    }
+
+    public static int fromOdo(float odo) {
+        return (int) ((odo - HALF) / ONE);
     }
 }
